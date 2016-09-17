57795e57a19a1aa88469718d22d9a6c7ce89f2ad
ernest <hunjae.lee@gmail.com>
2016-09-17 21:07:05 +0900

    enhance tuple2

--- app/src/main/java/com/example/android/calendartest6/Tuple2.java
+++ app/src/main/java/com/example/android/calendartest6/Tuple2.java
@@ -1,6 +1,7 @@
 package com.example.android.calendartest6;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by ernest on 16. 9. 17.
@@ -12,4 +13,19 @@ public class Tuple2<T, S> implements Serializable {
         this.t = t;
         this.s = s;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o != null && o instanceof Tuple2<?, ?>) {
+            Tuple2<?, ?> other = (Tuple2<?, ?>)o;
+            return Objects.equals(t, other.t) && Objects.equals(s, other.s);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(t, s);
+    }
+
 }
